b48d30780d70255396c80dc53bf8b5a40495908d
Daniel González Cortés <47788332+gonzalezcortes@users.noreply.github.com>
2023-01-31 18:18:55 +0100

    Modification in R. Plotting of results.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from scipy import stats
 import time 
 
@@ -82,3 +82,10 @@ class metrics:
         y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
         return mean_squared_error(y_true, y_pred)
 
+    def mae(y_true, y_pred):
+        y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+        return mean_absolute_error(y_true, y_pred)
+
+    def r2(y_true, y_pred):
+        y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+        return r2_score(y_true, y_pred)
